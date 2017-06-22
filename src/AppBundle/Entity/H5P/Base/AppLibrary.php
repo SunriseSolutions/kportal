131f45e158be686f2f0b47b28d12f21abdba18eb
Peter Pierre <binh@sunrise.vn>
2017-06-22 23:26:04 +0700

    - Implementing AppH5PFramework for H5P

--- src/AppBundle/Entity/H5P/Base/AppLibrary.php
+++ src/AppBundle/Entity/H5P/Base/AppLibrary.php
@@ -10,7 +10,7 @@ use JMS\Serializer\Annotation as Serializer;
 use Hateoas\Configuration\Annotation as Hateoas;
 
 /** @ORM\MappedSuperclass */
-class AppLibrary extends BaseGallery {
+class AppLibrary {
 	/**
 	 * @var int $id
 	 * @ORM\Id
@@ -19,25 +19,99 @@ class AppLibrary extends BaseGallery {
 	 * @ORM\CustomIdGenerator(class="AppBundle\Doctrine\ORM\RandomIdGenerator")
 	 */
 	protected $id;
-	//TODO Implement
 	
+	function __construct() {
+		$this->createdAt = new \DateTime();
+	}
+	
+	/**
+	 * @var \DateTime
+	 * @ORM\Column(type="datetime", options={"default": 0})
+	 */
 	protected $createdAt;
-	protected $updatedAt;
-	protected $machineName;
-	protected $title;
-	protected $majorVersion;
-	protected $minorVersion;
-	protected $patchVersion;
-	protected $runnable;
-	protected $restricted;
-	protected $fullscreen;
-	protected $embedTypes;
-	protected $preloadedJs;
-	protected $preloadedCss;
-	protected $dropLibraryCss;
-	protected $semantics;
-	protected $tutorialUrl;
-	protected $iconIncluded;
 	
+	/**
+	 * @var \DateTime
+	 * @ORM\Column(type="datetime", nullable=true)
+	 */
+	protected $updatedAt;
+	
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=127)
+	 */
+	protected $machineName;
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=255)
+	 */
+	protected $title;
+	/**
+	 * @var int
+	 * @ORM\Column(type="integer")
+	 */
+	protected $majorVersion;
+	/**
+	 * @var int
+	 * @ORM\Column(type="integer")
+	 */
+	protected $minorVersion;
+	/**
+	 * @var int
+	 * @ORM\Column(type="integer")
+	 */
+	protected $patchVersion;
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean", options={"default":false})
+	 */
+	protected $runnable = false;
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean", options={"default":false})
+	 */
+	protected $restricted = false;
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean", options={"default":false})
+	 */
+	protected $fullscreen = false;
+	
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=255)
+	 */
+	protected $embedTypes;
+	/**
+	 * @var string
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	protected $preloadedJs;
+	/**
+	 * @var string
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	protected $preloadedCss;
+	/**
+	 * @var string
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	protected $dropLibraryCss;
+	/**
+	 * @var string
+	 * @ORM\Column(type="text")
+	 */
+	protected $semantics;
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=1023)
+	 */
+	protected $tutorialUrl;
+	
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean", options={"default":false})
+	 */
+	protected $iconIncluded = false;
 	
 }
